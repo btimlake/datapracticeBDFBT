e6f1c1e72bd686224048d2dae9d12b4e544215c4
btimlake <ben@bentimberlake.com>
2016-04-14 14:50:33 +0200

    updated notes info

--- nadegedatapractice.m
+++ nadegedatapractice.m
@@ -1,1 +1,33 @@
-load Data_RegretEnvy.xlsx - Sheet1.csv
+load Data_RegretEnvy.csv
+
+csvread(/Users/bentimberlake/Documents/MATLAB/datapracticeBDFBT/Data_RegretEnvy.csv, 2, 1)
+
+data = Data_RegretEnvy.csv
+
+% Average decision times by
+    % age
+    
+    mean(
+    
+    % diff EV
+    
+    % trial type (competitive?)
+    
+    % education
+    
+    
+% Analysis
+% look at some averages of decision times
+% compare: age
+%      dEV
+%      competition
+%      education?
+% 
+% look at average rating for each type of outcome, then compare disappointment to regret
+% - average w/in subject
+% - compare across subjects
+% 
+% Effect of R/D on subsequent trial
+% 
+% Both: work with data in pivot tables and graphs; find some comparisons
+%      - Next week: figure out what to pursue in MATLAB
